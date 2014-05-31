1f62d3ea31bc73994a0d4fd5f2b071825df0877e
Andrea Kottas <andrea@atlanticdynamic.com>
2014-05-31 01:37:25 -0400

    add condition when  PE is not downloaded

--- pe-install/bootstrap.sh
+++ pe-install/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PE_VER='puppet-enterprise-3.2.3-el-6-x86_64'
-ANS_FILE=$1
+PE_VER=$1
+ANS_FILE=$2
+INSTALL_SOURCE="/vagrant/pe-install/${PE_VER}.tar.gz"
 
 if [ $UID != 0 ]
 then
@@ -15,6 +16,15 @@ then
     exit 0
 fi
 
+# If PE is not in the directory, show error
+if [ ! -f $INSTALL_SOURCE ]
+then
+  echo "File not found: $INSTALL_SOURCE"
+  echo "Please download download Puppet Enterprise x64 for CentOS 6."
+  exit 1
+fi
+
 cd /tmp
-tar -xzf "/vagrant/pe-install/${PE_VER}.tar.gz"
+tar -xzf $INSTALL_SOURCE
 /tmp/${PE_VER}/puppet-enterprise-installer -a $ANS_FILE
+rm -rf /tmp/${PE_VER}
